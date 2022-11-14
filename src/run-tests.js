7bd8205727eae3889e3726454b34ba41af65a9e0
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2022-11-14 18:39:45 +0200

    Refactor the channel logos and names test to run with Lambdatest

--- src/run-tests.js
+++ src/run-tests.js
@@ -1,5 +1,7 @@
 const { exampleTest } = require("./tests/example-test");
 const { emailFormValidation } = require("./tests/email_form_validation");
+const { logosAndNamesTest } = require("./tests/channel_logos_and_names");
 
 exampleTest();
 emailFormValidation();
+logosAndNamesTest();
